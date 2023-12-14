3061b6cdff8b8e2d66a24d657bd41c2079b5e23a
vishnupriya-22 <150262471+vishnupriya-22@users.noreply.github.com>
2023-12-14 18:39:47 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def navani(request):
+    return HttpResponse('we are not talking about the diary milk silk')
